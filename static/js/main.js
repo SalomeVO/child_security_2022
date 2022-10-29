01b2c339f3ccbc1f76ce66097aaef2949eebba08
SalomeVO <82300446+SalomeVO@users.noreply.github.com>
2022-10-28 23:15:51 -0600

    Update main.js

---
+++ static/js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+	$('[data-toggle="tooltip"]').tooltip();
+	// Animate select box length
+	var searchInput = $(".search-box input");
+	var inputGroup = $(".search-box .input-group");
+	var boxWidth = inputGroup.width();
+	searchInput.focus(function(){
+		inputGroup.animate({
+			width: "300"
+		});
+	}).blur(function(){
+		inputGroup.animate({
+			width: boxWidth
+		});
+	});
+});
+
+//Definicion de alerta personalizada
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+})
+
+/*
+Toast.fire({
+  icon: 'success',
+  title: 'Registrado con éxito'
+})
+*/
